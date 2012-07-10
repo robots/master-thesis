cf32596367338f964cfe28f05c40fc07cdcbea85
Michal Demin <michaldemin@gmail.com>
2012-07-10 10:17:54 +0200

    load gca script dynamicaly from gdbinit
    Signed-off-by: Michal Demin <michaldemin@gmail.com>

--- sw/rtems-run/.gdbinit
+++ sw/rtems-run/.gdbinit
@@ -1,5 +1,9 @@
 target remote 127.0.0.1:10000
 
+monitor gca_script load gca_rtems
+
 add-symbol-file hda/triple_period.exe 0x00100000
 
 info threads
+
+monitor gca print objects
--- sw/rtems-run/run.sh
+++ sw/rtems-run/run.sh
@@ -1,5 +1,8 @@
+source ../source-qemu
+
 export PYTHONPATH=./
 
-qemu-system-i386 -gca rtems -gdb tcp:localhost:10000 -m 128 -boot a -fda boot.img -hda fat:./hda --no-reboot
+qemu-system-i386 -gdb tcp:localhost:10000 -m 128 -boot a -fda boot.img -hda fat:./hda --no-reboot
+#qemu-system-i386 -gca gca_rtems -gdb tcp:localhost:10000 -m 128 -boot a -fda boot.img -hda fat:./hda --no-reboot
 
 #qemu-i386 -gdb tcp:localhost:10000 -m 128 -boot a -fda boot.img -hda fat:./hda --no-reboot
